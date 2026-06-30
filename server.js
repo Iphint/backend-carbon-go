50fd86800e85d17a48bef01549b74b3cb3f5ce39
iphint <ma7804058@gmail.com>
2026-06-30 09:38:49 +0700

    updated

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Carbon-Go API running on http://localhost:${port}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
 });
